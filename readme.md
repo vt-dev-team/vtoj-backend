5f8253c755a31e523474373e9164f3b9a7887a46
yemaster <yemaster@oigat.cn>
2024-07-13 14:42:56 +0800

    Update Comment

--- readme.md
+++ readme.md
@@ -1,5 +1,10 @@
 ### VTOJ-backend
 
+#### 技术栈
+
+- 语言：TypeScript
+- 框架：Fastify、TypeORM
+
 #### 安装
 
 ```
@@ -10,3 +15,22 @@ yarn
 yarn serve
 ```
 
+#### 目录结构
+
+- models
+  存放TypeORM的Entity实体。不小心把一些常数也放进去了
+- routes
+  存放路由，再在index.ts导出。实际上，不只是路由，把逻辑啥的都写了
+- data
+  存放题目的测试数据，可以在config里面修改
+- hooks
+  fastify的路由钩子，不需要改。onSend是把json的格式套层壳。
+- plugins
+  fastify注册的插件，放了typeorm插件。
+- middlewares
+  中间件，权限验证感觉要在这里完成
+- utils
+- server.ts
+  核心程序
+- config.json
+  核心配置
--- src/server.ts
+++ src/server.ts
@@ -12,19 +12,20 @@ import routes from './routes';
 import { onSend } from './hooks/onSend';
 
 const app = Fastify({
-    logger: true
+    logger: process.env.NODE_ENV !== 'production'
 });
 
-// Register the typeorm plugin with the database configuration
+// 注册TypeORM的插件，数据库的设置在config.json中配置
 app.register(typeorm, {
     ...db,
     entities: [Announcement, User, Problem, ChooseProblem, Submission, Domain, Contest],
     synchronize: true
 });
 
-// Register cookie and session plugins
+// 注册Cookie和Session插件
 app.register(fastifyCookie);
 app.register(fastifySession, {
+    // 生成一个随机的session secret，理论上还是弄一个环境变量比较好
     secret: process.env.SESSION_SECRET || randomBytes(32).toString('hex'),
     cookie: {
         secure: process.env.NODE_ENV === 'production',
@@ -34,12 +35,13 @@ app.register(fastifySession, {
     saveUninitialized: false
 });
 
+// 注册onSend钩子，会把json数据转成ResponseFormatter设定的格式
 app.addHook('onSend', onSend);
 
-// Register routes with the /api prefix
+// 注册路由，所有的路由都在routes文件夹中，并加上前缀/api
 app.register(routes, { prefix: "/api" });
 
-// Run the server!
+// 启动服务器
 app.listen(server, function (err, address) {
     if (err) {
         app.log.error(err)
